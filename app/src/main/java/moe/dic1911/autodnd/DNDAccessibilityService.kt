e47923975bc42d45dfcd72b1f9fb4de8c0ff5f10
030 <030@notfound.404>
2021-09-12 21:04:42 +0800

    cleanup accessibility service code

--- app/src/main/java/moe/dic1911/autodnd/DNDAccessibilityService.kt
+++ app/src/main/java/moe/dic1911/autodnd/DNDAccessibilityService.kt
@@ -11,7 +11,6 @@ import android.media.AudioManager
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityEvent.*
-import androidx.lifecycle.Observer
 import moe.dic1911.autodnd.data.Storage
 
 
@@ -39,21 +38,10 @@ class DNDAccessibilityService : AccessibilityService() {
 
         bakRingMode = (getSystemService(Context.AUDIO_SERVICE) as AudioManager).ringerMode
 
-        /*val recv_noti: BroadcastReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                if (state == 1) return
-                bakNotiState = (getSystemService(Context.NOTIFICATION_SERVICE)
-                        as NotificationManager).currentInterruptionFilter
-                Log.d("030_recv_noti", "noti/dnd: $bakNotiState, ring: $bakRingMode")
-            }
-        }
-        val filter_noti = IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION)
-        //registerReceiver(recv_noti, filter_noti)*/
-
         val recv_ringer: BroadcastReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
-                val dnd = (getSystemService(Context.NOTIFICATION_SERVICE)
-                        as NotificationManager).currentInterruptionFilter
+                val notiMan = (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                val dnd = notiMan.currentInterruptionFilter
                 if (state == 1 || dnd != 1) {
                     Log.d("030_recv_ring", "Either we're running apps in list or DND is on/unknown")
                     return
@@ -78,34 +66,23 @@ class DNDAccessibilityService : AccessibilityService() {
             val newCurApp = event.packageName?.toString() ?: return // TODO: NPE?
             if (newCurApp != curApp && !blacklist.contains(newCurApp)) {
                 curApp = newCurApp
-                //Log.d("030_ev+app", "${eventTypeToString(event.eventType)}, current app = $curApp")
+                Log.d("030_ev+app", "${eventTypeToString(event.eventType)}, current app = $curApp")
                 appList = Storage.prefs_str.value!!
 
                 if (appList.contains(curApp)) {
-
                     bakNotiState = notiMan.currentInterruptionFilter
-                    //bakRingMode = am.ringerMode // use value we had obtained earlier instead
-
-                    //Log.d("030_preautodnd", "noti/dnd: $bakNotiState, ring: $bakRingMode")
                     if (bakNotiState == 1)
                         notiMan.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALARMS)
                     state = 1
                 } else if (state == 1) {
                     state = 0
                     if (bakNotiState != notiMan.currentInterruptionFilter) {
-                        //notiMan.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL)
                         notiMan.setInterruptionFilter(bakNotiState)
-                        am.ringerMode = bakRingMode //AudioManager.RINGER_MODE_VIBRATE
+                        am.ringerMode = bakRingMode
                     }
                 }
             }
-            /*Log.d("030", AccessibilityEvent.eventTypeToString(event.eventType))
-            if (event.eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
-                Log.d("030", event.packageName.toString())
-            }*/
-        }/* else {
-            Log.d("030", "Null event!")
-        }*/
+        }
     }
 
 }
